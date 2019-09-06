b76ddf80e8c7a152ff0bbf79fdfb21df8e12f381
jico <u4m062@gmail.com>
2019-09-06 15:19:45 +0800

    ListItemSwitch 调整

--- src/components/ListItem/ListItemSwitch.js
+++ src/components/ListItem/ListItemSwitch.js
@@ -2,13 +2,21 @@ import React from "react";
 import propTypes from "prop-types";
 import { ListItem } from "react-native-elements";
 
-const ListItemSwitch = ({ title, value, ...props }) => {
-  return <ListItem title={title} switch={{ value }} bottomDivider {...props} />;
+const ListItemSwitch = ({ title, value, setValue, ...props }) => {
+  return (
+    <ListItem
+      title={title}
+      switch={{ value, onValueChange: setValue }}
+      bottomDivider
+      {...props}
+    />
+  );
 };
 
 ListItemSwitch.propTypes = {
   title: propTypes.string,
-  value: propTypes.bool
+  value: propTypes.bool,
+  setValue: propTypes.func
 };
 
 export default ListItemSwitch;

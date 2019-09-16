611e4321a5172bf2b65ec9556a67339e5f2146c8
jico <u4m062@gmail.com>
2019-09-16 14:19:07 +0800

    我的帳號頁面調整

--- src/screens/AccountScreen/index.js
+++ src/screens/AccountScreen/index.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
-import { Text, ListItem } from "react-native-elements";
-import { ListItemSwitch, ListItemLink } from "../../components/ListItem";
+import { Alert } from "react-native";
 import { Actions } from "react-native-router-flux";
+import { Text, ListItem } from "react-native-elements";
+import {
+  ListItemSwitch,
+  ListItemLink,
+  ListItemChat
+} from "../../components/ListItem";
 import ViewBox from "../../components/ViewBox";
 
 const userDetail = {
+  id: 123132154,
   name: "Wendy",
   avatar_url: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
-  id: "1231321546"
+  edit: true,
+  msgNotice: true,
+  noticeDetail: false
 };
 
 const AccountScreen = () => {
-  const [msgNotice, setMsgNotice] = useState(true);
-  const [noticeDetail, setNoticeDetail] = useState(false);
+  const [msgNotice, setMsgNotice] = useState(userDetail.msgNotice);
+  const [noticeDetail, setNoticeDetail] = useState(userDetail.noticeDetail);
 
   const toQRcode = () => {
     Actions.myCode();
@@ -27,28 +35,43 @@ const AccountScreen = () => {
   };
 
   const handleLogout = () => {
-    Actions.replace("login", {});
+    Alert.alert(
+      "确定退出登录？",
+      "",
+      [
+        {
+          text: "取消",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "退出", onPress: () => Actions.replace("login", {}) }
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const handleMsgNotice = () => {
+    setMsgNotice(prevMsgNotice => !prevMsgNotice);
+  };
+
+  const handleNotices = () => {
+    setNoticeDetail(prevNoticeDetail => !prevNoticeDetail);
   };
 
   return (
     <ViewBox fill>
-      <ListItem
-        title={userDetail.name}
-        leftAvatar={{ source: { uri: userDetail.avatar_url } }}
-        bottomDivider
-        onPress={toEditName}
-      />
-      <ListItem title='ID' rightTitle={userDetail.id} bottomDivider />
+      <ListItemChat data={userDetail} onPress={toEditName} />
+      <ListItem title='ID' rightTitle={String(userDetail.id)} bottomDivider />
       <ListItemLink title='我的二维码' icon onPress={toQRcode} />
       <ListItemSwitch
         title='新消息通知'
         value={msgNotice}
-        setValue={() => setMsgNotice(!msgNotice)}
+        setValue={handleMsgNotice}
       />
       <ListItemSwitch
         title='通知显示详情'
         value={noticeDetail}
-        setValue={() => setNoticeDetail(!noticeDetail)}
+        setValue={handleNotices}
       />
       <Text h4>其他</Text>
       <ListItemLink title='黑名单' small icon onPress={toBlock} />

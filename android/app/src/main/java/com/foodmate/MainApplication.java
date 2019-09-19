c8330337af3f92e00ad623962c483b0e70a009a3
林暐秦（Tomas） <tomas.lin@velocitylight.net>
2019-09-19 18:17:46 +0800

    add react-native-config add base64-js

--- android/app/src/main/java/com/foodmate/MainApplication.java
+++ android/app/src/main/java/com/foodmate/MainApplication.java
@@ -3,6 +3,7 @@ package com.foodmate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import cn.jpush.reactnativejpush.JPushPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new JPushPackage(!BuildConfig.DEBUG, !BuildConfig.DEBUG)

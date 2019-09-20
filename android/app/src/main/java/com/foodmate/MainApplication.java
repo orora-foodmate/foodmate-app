8953d61bb4798ac0a7abb642fefb9d17fad133e2
林暐秦（Tomas） <tomas.lin@velocitylight.net>
2019-09-20 18:36:47 +0800

    update imapp => foodmate

--- android/app/src/main/java/com/foodmate/MainApplication.java
+++ android/app/src/main/java/com/foodmate/MainApplication.java
@@ -1,10 +1,10 @@
-package com.imapp;
+package com.foodmate;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import io.realm.react.RealmReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
             new RealmReactPackage(),
+            new VectorIconsPackage(),
             new ReactNativeConfigPackage()
       );
     }
--- ios/foodmateTests/foodmateTests.m
+++ ios/foodmateTests/foodmateTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface imappTests : XCTestCase
+@interface foodmateTests : XCTestCase
 
 @end
 
-@implementation imappTests
+@implementation foodmateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {

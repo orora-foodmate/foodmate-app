412993f9f9e120001582caadd31ed2a99fb93071
林暐秦（Tomas） <tomas.lin@velocitylight.net>
2019-09-20 15:17:39 +0800

    重新建立一個新的 project

--- android/app/src/main/java/com/imapp/MainApplication.java
+++ android/app/src/main/java/com/imapp/MainApplication.java
@@ -3,8 +3,9 @@ package com.imapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import io.realm.react.RealmReactPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,8 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RealmReactPackage(),
-            new VectorIconsPackage()
+            new ReactNativeConfigPackage()
       );
     }
 

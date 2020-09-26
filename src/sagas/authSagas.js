44d83b4745f7dcdb50ff2e4b7863b9b04284b129
Tomas Lin <tomas.lin@velocitylight.net>
2020-09-26 17:26:44 +0800

    update react native to 0.63.2

--- src/sagas/authSagas.js
+++ src/sagas/authSagas.js
@@ -1,6 +1,5 @@
 import { put, call } from 'redux-saga/effects';
 import types from '../constants/actionTypes';
-import auth from '@react-native-firebase/auth';
 import { getTokenResult } from '../apis/api';
 
 const okLogin = (confirmation) => ({
@@ -16,18 +15,13 @@ const errLogin = ({ message, status }) => {
   };
 };
 
-const firebaseSignInWithPhoneNumber = (phoneNumber) => {
-  return auth().signInWithPhoneNumber(phoneNumber);
-};
 export function* loginSaga({ payload }) {
   try {
     console.log("TCL: function*loginSaga -> payload", payload)
     const result = yield call(getTokenResult, payload);
-    console.log('%c%s', 'color: #e50000', result);
 
     yield put(okLogin());
   } catch (error) {
-    console.log('%c%s', 'color: #733d00', error);
     const errorAction = errLogin(error);
     yield put(errorAction);
   }
@@ -49,9 +43,8 @@ const errGet = ({ message, status }) => {
 
 export function* getConfirmCodeSaga({ payload }) {
   try {
-    const confirmation = yield firebaseSignInWithPhoneNumber(payload.phoneNumber);
 
-    yield put(okGet(confirmation));
+    yield put(okGet({}));
   } catch (error) {
     const errorAction = errGet(error);
     yield put(errorAction);

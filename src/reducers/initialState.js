20ae7c0ad652faa18553996b97c585ee50a05c2c
Tomas Lin <tomas.lin@velocitylight.net>
2020-10-15 15:22:52 +0800

    登入 建立 socket 初始化 Database
    登出
    DB 資料清除
    socket 斷線

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -2,6 +2,7 @@ import { fromJS } from 'immutable';
 
 export const authState = fromJS({
   isAuth: false,
+  socket: null,
   account: '',
   avatar: '',
   token: '',
--- src/sagas/initialAppSaga.js
+++ src/sagas/initialAppSaga.js
@@ -1,9 +1,8 @@
 import types from '~/constants/actionTypes';
-import socketClusterClient from 'socketcluster-client';
-import Config from '~/constants/envConfig';
 import { call, put } from 'redux-saga/effects';
 import { initSQL } from '~/models';
 import { getLoginUser, validateIsFirstLaunch } from '~/helper/authHelpers';
+import socketClusterHelper from '~/helper/socketClusterHelpers';
 
 const okInitial = (payload) => ({
   type: types.INITIAL_APP_SUCCESS,
@@ -20,44 +19,17 @@ export function* initialAppSaga() {
     const isFirstLaunch = yield call(validateIsFirstLaunch);
     const loginUser = yield call(getLoginUser);
     const database = yield call(initSQL, isFirstLaunch);
-    const socket = socketClusterClient.create({
-      secure: false,
-      hostname: Config.hostname,
-      port: Config.port,
-      authTokenName: 'socketcluster.authToken',
-      autoReconnectOptions: {
-        initialDelay: 10000, //milliseconds
-        randomness: 10000, //milliseconds
-        multiplier: 1.5, //decimal
-        maxDelay: 60000 //milliseconds
-      },
-      authEngine: {
-        _internalStorage: {
-          "socketcluster.authToken": loginUser.token
-        },
-        isLocalStorageEnabled: true,
-        saveToken: (name, token, options) => {
-          console.log('function*initialAppSaga -> token', token)
-          console.log('function*initialAppSaga -> name', name)
-          this._internalStorage[name] = token;
-          return Promise.resolve(token);
-        },
-        removeToken: function (name) {
-          const loadPromise = this.loadToken(name);
-          delete this._internalStorage[name];
-          return loadPromise;
-        },
-        loadToken: function (name) {
-          const token = this._internalStorage[name] || null;
-          return Promise.resolve(token);
-        }
-      }
-    });
-    console.log('function*initialAppSaga -> socket', socket)
-    const resAction = true ? okInitial({ database }) : errInitial();
+
+    const socket = loginUser.isAuth
+      ? socketClusterHelper.initialClient(loginUser.token)
+      : null;
+
+    const resAction = true
+      ? okInitial({ socket, database })
+      : errInitial();
     yield put({
       type: types.SET_LOGIN_USER,
-      payload: loginUser,
+      payload: { ...loginUser, socket },
     });
 
     yield put(resAction);

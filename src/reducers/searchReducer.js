0a2002592c54fef490bd46c0ba5b48f572d48c6a
Edmond Wu <edmond.wu@velocitylight.net>
2020-12-02 18:03:43 +0800

    調整好友文案

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -4,6 +4,7 @@ import {fromJS} from 'immutable';
 import {searchState} from './initialState';
 
 export default function reducer(search = searchState, {type, payload}) {
+  console.log("TCL ~ file: searchReducer.js ~ line 19 ~ reducer ~ payload", payload)
   switch (type) {
     case types.APPROVE_INVITE_FRIEND_SUCCESS:
       return search.update('user', (user) => user.merge({status: 2}));
@@ -15,7 +16,7 @@ export default function reducer(search = searchState, {type, payload}) {
       return search.update('user', (user) => user.merge(payload));
     case types.CLEAR_SEARCH_FRIEND_RESULT:
     case types.GET_USER_BY_ACCOUNT_SUCCESS:
-      if(isEmpty(payload)) return search;
+      if(isEmpty(payload)) return searchState;
       return search.update('user', () => fromJS(payload));
     case types.APPROVE_INVITE_FRIEND:
     case types.APPROVE_INVITE_FRIEND_ERROR:

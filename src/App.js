24f7c18dfbc116f0b27e73cf341168a1ef3a44b4
Tomas Lin <horsekit1982@gmail.com>
2020-12-08 22:01:46 +0800

    修正 Overlay 只能有 Single Element 的錯誤

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {ThemeProvider} from 'react-native-elements';
-import theme from './theme';
 import {NavigationProvider} from 'react-native-navigation-hooks';
-import store from './store/configureStore';
+import theme from '~/theme';
+import store from '~/store/configureStore';
+import ScrollContainer from '~/components/ScrollContainer';
 
 const App = (Component) => (props) => {
   return (
-    <Provider store={store}>
-      <NavigationProvider value={{componentId: props.componentId}}>
-        <ThemeProvider theme={theme}>
-          <Component {...props} />
-        </ThemeProvider>
-      </NavigationProvider>
-    </Provider>
+    <ScrollContainer>
+      <Provider store={store}>
+        <NavigationProvider value={{componentId: props.componentId}}>
+          <ThemeProvider theme={theme}>
+            <Component {...props} />
+          </ThemeProvider>
+        </NavigationProvider>
+      </Provider>
+    </ScrollContainer>
   );
 };
 

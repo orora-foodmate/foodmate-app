95ba61a944ab6dc0ca39d98fe2564827ca0cd36e
Edmond Wu <edmond.wu@velocitylight.net>
2020-12-22 14:48:52 +0800

    調整聊天室樣式

--- src/screens/ChatScreen/view.js
+++ src/screens/ChatScreen/view.js
@@ -1,7 +1,47 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { View } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import isEmpty from 'lodash/isEmpty';
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { GiftedChat, Bubble, Send } from 'react-native-gifted-chat';
+import { Icon } from 'react-native-elements';
 import { useMessages } from '~/models';
+import colors from '~/theme/color';
+
+const renderBubble = (props) => {
+  return (
+    <TouchableWithoutFeedback>
+      <View style={{ flex: 1 }}>
+        <Bubble
+          {...props}
+          wrapperStyle={{
+            left: styles.messageBubbleLeft,
+            right: styles.messageBubbleRight,
+          }}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const SendButton = (props) => {
+  const buttonColor = isEmpty(props.text) ? '#707070' : colors.primary;
+
+  return (
+    <Send {...props}>
+      <Icon
+        containerStyle={{
+          width: 40,
+          height: 54,
+          bottom: -10,
+          paddingLeft: 0,
+          paddingTop: 10,
+        }}
+        color={buttonColor}
+        name='send'
+        type='materialicon'
+      />
+    </Send>
+  );
+};
 
 const ChatScreen = ({ userId, roomId, handleAddMessage, handleGetMessages }) => {
   const messages = useMessages();
@@ -24,6 +64,9 @@ const ChatScreen = ({ userId, roomId, handleAddMessage, handleGetMessages }) => 
       <GiftedChat
         style={{ flex: 1 }}
         messages={messages}
+        placeholder='请输入文字讯息'
+        renderBubble={renderBubble}
+        renderSend={(sendProps) => <SendButton {...sendProps} />}
         onQuickReply={(value) => console.log(`onQuickReply:`, value)}
         onSend={(messages) => onSend(messages)}
         user={{
@@ -33,5 +76,16 @@ const ChatScreen = ({ userId, roomId, handleAddMessage, handleGetMessages }) => 
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  messageBubbleLeft: {
+    backgroundColor: 'white',
+    borderColor: '#ddd',
+    borderWidth: 1,
+  },
+  messageBubbleRight: {
+    backgroundColor: colors.primary,
+  },
+})
 
 export default ChatScreen;
--- src/screens/RoomsScreen/components/RoomItem.js
+++ src/screens/RoomsScreen/components/RoomItem.js
@@ -1,19 +1,42 @@
-import React, { useEffect } from 'react';
-import { ListItem, Avatar } from 'react-native-elements';
+import React, {useEffect, Fragment} from 'react';
+import {StyleSheet} from 'react-native';
+import {ListItem, Icon, Avatar, colors} from 'react-native-elements';
 
-async function asyncIteratorWatcher(channel, userId, handleAddMessageByWebsocket) {
+async function asyncIteratorWatcher(
+  channel,
+  userId,
+  handleAddMessageByWebsocket
+) {
   let asyncIterator = channel.createConsumer();
   while (true) {
     let packet = await asyncIterator.next();
     if (packet.done) break;
     const {user} = packet.value;
-    if(user.id !== userId) {
+    if (user.id !== userId) {
       handleAddMessageByWebsocket(packet.value);
     }
   }
 }
 
-const RoomItem = ({ socket, userId, title, subTitle, avatar, roomId, type, push, handleAddMessageByWebsocket }) => {
+const TypeIcon = ({type}) => {
+  if (type !== 'event') return <Fragment />;
+
+  return (
+    <Icon size={12} type='font-awesome' name='users' color={colors.grey4} style={styles.icon} />
+  );
+};
+
+const RoomItem = ({
+  socket,
+  userId,
+  title,
+  subTitle,
+  avatar,
+  roomId,
+  type,
+  push,
+  handleAddMessageByWebsocket,
+}) => {
   useEffect(() => {
     const channel = socket.subscribe(`room.newMessage.${roomId}`);
     asyncIteratorWatcher(channel, userId, handleAddMessageByWebsocket);
@@ -21,14 +44,28 @@ const RoomItem = ({ socket, userId, title, subTitle, avatar, roomId, type, push,
   }, []);
 
   return (
-    <ListItem bottomDivider onPress={() => push("Chat", { roomId, type })}>
-      <Avatar source={{ uri: avatar }} />
+    <ListItem bottomDivider onPress={() => push('Chat', {roomId, type})}>
+      <Avatar rounded source={{uri: avatar}} />
       <ListItem.Content>
-        <ListItem.Title>{title}</ListItem.Title>
-        <ListItem.Subtitle>{subTitle}</ListItem.Subtitle>
+        <ListItem.Title>
+          {title}
+          <TypeIcon type={type} />
+        </ListItem.Title>
+        <ListItem.Subtitle style={styles.subtitle}>
+          {subTitle}
+        </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
   );
 };
+
+const styles = StyleSheet.create({
+  subtitle: {
+    color: colors.grey3,
+  },
+  icon: {
+    marginLeft: 5
+  }
+});
 
 export default RoomItem;

0b57e57a8cda91872062fcb0a090783001a1df49
Tomas Lin <tomas.lin@velocitylight.net>
2020-12-28 15:32:32 +0800

    update pre build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env bash
+if [ ! -z "$GOOGLE_SERVICES_JSON" ]; then
+  echo $GOOGLE_SERVICES_JSON | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/android/app/google-services.json"
+fi
 
-yarn install
-./node_modules/gulp/bin/gulp.js default
+if [ ! -z "$GOOGLE_SERVICES_PLIST" ]; then
+  echo $GOOGLE_SERVICES_PLIST | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/react-native-starter/GoogleService-Info.plist"
+fi

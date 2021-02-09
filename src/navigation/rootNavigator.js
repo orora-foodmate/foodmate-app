12f0dc065c52da9cd2f41096d37894045bdda5f6
mina <mina.tsai@velocitylight.net>
2021-02-09 11:22:19 +0800

    update navigation default options

--- src/navigation/rootNavigator.js
+++ src/navigation/rootNavigator.js
@@ -1,65 +1,57 @@
-import Feather from 'react-native-vector-icons/Feather';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {Navigation} from 'react-native-navigation';
 import colors from '~/theme/color';
 import {stack} from './stack';
 
-Feather.loadFont();
-
-Navigation.setDefaultOptions({
-  statusBar: {
-    backgroundColor: colors.primary,
-  },
-  topBar: {
-    title: {
-      color: 'white',
-    },
-    backButton: {
-      color: 'white',
-    },
-    background: {
-      color: colors.primary,
-    },
-  },
-  bottomTab: {
-    fontSize: 14,
-    selectedFontSize: 18,
-    selectedIconColor: colors.primary,
-    selectedTextColor: colors.primary,
-  },
-});
-
 const rootNavigator = () => {
   Promise.all([
-    MaterialIcons.getImageSource('add-circle', 25, colors.primary),
-    MaterialIcons.getImageSource('add-circle', 25, colors.greyLight),
-    MaterialIcons.getImageSource('forum', 25, colors.primary),
-    MaterialIcons.getImageSource('forum', 25, colors.greyLight),
-    MaterialIcons.getImageSource('person-pin', 25, colors.primary),
-    MaterialIcons.getImageSource('person-pin', 25, colors.greyLight),
-    MaterialIcons.getImageSource('settings', 25, colors.primary),
-    MaterialIcons.getImageSource('settings', 25, colors.greyLight),
+    MaterialIcons.getImageSource('add-circle', 25),
+    MaterialIcons.getImageSource('forum', 25),
+    MaterialIcons.getImageSource('person-pin', 25),
+    MaterialIcons.getImageSource('settings', 25),
   ]).then(
     ([
-      createIconActive,
       createIcon,
-      chatIconActive,
       chatIcon,
-      usersIconActive,
       usersIcon,
-      settingIconActive,
       settingIcon,
     ]) => {
+
+      Navigation.setDefaultOptions({
+        statusBar: {
+          backgroundColor: colors.primary,
+        },
+        topBar: {
+          title: {
+            color: 'white',
+          },
+          backButton: {
+            color: 'white',
+          },
+          background: {
+            color: colors.primary,
+          },
+        },
+        bottomTab: {
+          fontSize: 12,
+          iconColor: colors.greyLight,
+          textColor: colors.greyLight,
+          selectedIconColor: colors.primary,
+          selectedTextColor: colors.primary,
+        },
+      });
+      
+
       Navigation.setRoot({
         root: {
           sideMenu: {
             center: {
               bottomTabs: {
                 children: [
-                  stack('Events', '活動列表', createIconActive, createIcon),
-                  stack('Friend', '朋友圈', usersIconActive, usersIcon),
-                  stack('Rooms', '聊天', chatIconActive, chatIcon),
-                  stack('Profile', '設置', settingIconActive, settingIcon),
+                  stack('Events', '活動列表', createIcon),
+                  stack('Friend', '朋友圈', usersIcon),
+                  stack('Rooms', '聊天', chatIcon),
+                  stack('Profile', '設置', settingIcon),
                 ],
               },
             },
